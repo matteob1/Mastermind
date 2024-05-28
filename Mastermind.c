457c2da411b9ecbbeb8cffd655d54e12c10650d1
matteob1 <matteob1@outlook.it>
2024-05-28 17:31:04 +0200

    primi passi

--- Mastermind.c
+++ Mastermind.c
@@ -6,14 +6,14 @@
 
   1. Il codificatore crea un codice segreto di lunghezza fissa, composto da una sequenza di elementi.
      Gli elementi possono essere colori, lettere o numeri e possono ripetersi.
-  
+
   2. Il decodificatore fa una serie di tentativi per cercare di indovinare il codice segreto.
      Ogni tentativo è una sequenza della stessa lunghezza del codice segreto.
-  
+
   3. Dopo ogni tentativo, il codificatore fornisce un feedback sotto forma di due numeri:
      - Punteggi neri: indicano quanti elementi del tentativo sono corretti sia per valore che per posizione.
      - Punteggi bianchi: indicano quanti elementi del tentativo sono corretti per valore ma non per posizione.
-  
+
   4. Il gioco continua finché il decodificatore non indovina esattamente il codice segreto o finché non esaurisce
      i tentativi a disposizione.
 
@@ -26,7 +26,7 @@
     * Difficile: 4 tentativi
 
   - Durante la partita, l'utente avrà la possibilità di abbandonare il gioco e tornare al menù principale.
-  
+
   - Se un utente vince la partita, verrà incrementato il contatore delle partite vinte dall'inizio della sessione e riceverà
     10 punti per ogni partita vinta. Se perde, comparirà una scritta "Game Over" e potrà scegliere se riprovare la partita con
     gli stessi settaggi della precedente oppure tornare al menù principale.
@@ -44,19 +44,36 @@
   points= n
 */
 
-
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define FACILE 8
 #define INTERMEDIA 6
 #define DIFFICILE 4
+#define ROSSO 1
+#define VERDE 2
+#define BLU 3
+#define GIALLO 4
+#define MARRONE 5
+#define ARANCIONE 6
+#define NERO 7
+#define BIANCO 8
 
+int main()
+{
 
-int main (){
+  char id_utente[11]; // Buffer per 10 caratteri + terminatore nullo (\0) che viene aggiunto automaticamente alla fine dello scanf
+  printf("****** BENVENUTO IN MASTERMIND ******\n INSERISCI IL TUO ID UTENTE:\n");
 
+  // Usa fgets per leggere la stringa, inclusi gli spazi
+  if (fgets(id_utente, sizeof(id_utente), stdin) != NULL)
+  // Il terzo parametro di fgets in questo caso è 'stdin', che indica che l'input proviene dallo standard input, cioè dalla tastiera.
+  {
+    // Rimuove il newline finale, se presente
+    id_utente[strcspn(id_utente, "\n")] = '\0';
+  }
 
-
-    return 0;
+  printf("Stringa letta: '%s'\n", id_utente);
+  return 0;
 }
