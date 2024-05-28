613937ee6fcd9f2ba348dab1dbceab7572a2411e
matteob1 <94492993+matteob1@users.noreply.github.com>
2024-05-28 22:21:20 +0200

    aggiunta funzione verifica_id da testare

--- Mastermind.c
+++ Mastermind.c
@@ -37,16 +37,17 @@
 
   La struttura dei dati contenuti nel file per singolo giocatore sarà la seguente:
 
-  . // separatore
   id= id utente // max 10 caratteri
   games-played= n
   games-won= n
   points= n
+  . // separatore
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #define FACILE 8
 #define INTERMEDIA 6
@@ -69,13 +70,79 @@ typedef struct struttura_giocatore
   
 } giocatore;
 
-int verifica_id (char id [11], char nome_file[]);
+
+
+int strcasecmp(const char *a, const char *b) { //funzione che dati due puntatori a costanti a e b li confronta in maniera non case sensitive
+    while (*a && *b) { //Il ciclo while (*a && *b) continua finché entrambe le stringhe non raggiungono il terminatore di stringa ('\0') 
+        if (tolower((unsigned char)*a) != tolower((unsigned char)*b)) {  //con le funzioni tolower porto i caratteri in minuscolo e se sono diversi (hanno un valore ascii diverso)
+            return tolower((unsigned char)*a) - tolower((unsigned char)*b);// restituisco il valore della loro differenza
+        }
+        a++; //carattere successivo di a (incremento del puntatore)
+        b++; //carattere successivo di b (incremento del puntatore)
+    }
+    return tolower((unsigned char)*a) - tolower((unsigned char)*b); // restituisco il valore della loro differenza (se uguali 0)
+}
+
+
+void verifica_id (giocatore player, char id [11], char nomefile[]){
+
+  FILE *fp;
+  char id_tmp [11];
+  fp = fopen(nomefile,"r");//apertura file in modalità lettura
+   if (fp == NULL) {
+		printf("errore apertura file \n");
+		exit(1);
+	}
+     // Leggi il file blocco per blocco
+    while (fscanf(fp, "id= %10s\n", id_tmp) == 1 && !feof(fp)) { // legge finchè non trova un campo id e finchè non trova il campo eof (end of file)
+        if (strcasecmp(id, id_tmp) == 0) { //chiamo la funzione di confronto non case sensitive, se il valore di ritorno è 0 allora leggo e salvo dati utente
+            // Trovato l'utente, leggi le informazioni
+            strcpy(player.id,id_tmp); // siccome non è possibile passare direttamente l'array id_tmp all'id del player lo copiamo con strcpy
+            fscanf(fp, "games-played= %d\n", &player.partite_giocate);
+            fscanf(fp, "games-won= %d\n", &player.partite_vinte);
+            fscanf(fp, "points= %d\n", &player.punti);
+            fclose(fp);
+            printf("Utente trovato, sono stati recuperati i dati delle sessioni precedenti\n");
+
+        } else {
+            // Salta il blocco di 3 righe e il separatore "."
+            fscanf(fp, "games-played= %*d\n");  // nella fscanf il "*"" indica che vuoi saltare il valore letto e non assegnarlo a nessuna variabile.
+            fscanf(fp, "games-won= %*d\n");
+            fscanf(fp, "points= %*d\n");
+            while (fgetc(fp) != '.' && !feof(fp)); 
+            fgetc(fp); // Legge il carattere '\n' dopo il '.'
+        }
+    }
+    fclose(fp);
+
+    // arrivati a questo punto vuol dire che l'utente non esiste e bisogna crearlo
+    char consenso;
+    printf("L'id utente da te inserito non è presente nella memoria.\n Vuoi creare un nuovo profilo?\n y/n");
+    if(getc(consenso)== 'y'){
+      fp=fopen(nomefile,"a"); //apertura file in modalità append
+      strcpy(player.id,id);
+      fprintf("id= %10s\n",id);
+      player.partite_giocate, player.partite_vinte, player.punti = 0;
+      fprintf("games-played= %d\n",0);
+      fprintf("games-won= %d\n",0);
+      fprintf("points= %d\n",0);
+      fprintf(".\n");
+      fclose(fp);
+      system(clear);
+      printf("Il tuo nuovo profilo utente è stato creato correttamente!!");
+    }
+    else {
+      system(clear);
+      printf("Alla prossima!!");
+      exit(1);
+    }
+}
 
 
 
 int main()
 {
-
+  giocatore player;
   char id_utente[11]; // Buffer per 10 caratteri + terminatore nullo (\0) che viene aggiunto automaticamente alla fine dello scanf
   printf("****** BENVENUTO IN MASTERMIND ******\n INSERISCI IL TUO ID UTENTE:\n");
 
@@ -89,7 +156,7 @@ int main()
     come secondo parametro ("\n"). Ha come valore di ritorno il contatore di posizioni analizzate a partire da zero.
     Quindi abbiamo preso l'elemento dell'array "id_utente" nella posizione del carattere \n (se presente) e lo sostituiamo con il carattere nullo*/
   }
-
+  
   /*printf("Stringa letta: '%s'\n", id_utente);*/
 
   return 0;
