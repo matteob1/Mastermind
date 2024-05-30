b38b8314adbab52ab37ea6fef644bf142cd99152
matteob1 <matteob1@outlook.it>
2024-05-30 23:06:22 +0200

    modificata la funzione strcasecmp

--- Mastermind.c
+++ Mastermind.c
@@ -1,22 +1,4 @@
 /*
-  Regole del gioco Mastermind:
-
-  Mastermind è un gioco di decodifica in cui un giocatore (il codificatore) crea un codice segreto,
-  e l'altro giocatore (il decodificatore) cerca di indovinarlo entro un certo numero di tentativi.
-
-  1. Il codificatore crea un codice segreto di lunghezza fissa, composto da una sequenza di elementi.
-     Gli elementi possono essere colori, lettere o numeri e possono ripetersi.
-
-  2. Il decodificatore fa una serie di tentativi per cercare di indovinare il codice segreto.
-     Ogni tentativo è una sequenza della stessa lunghezza del codice segreto.
-
-  3. Dopo ogni tentativo, il codificatore fornisce un feedback sotto forma di due numeri:
-     - Punteggi neri: indicano quanti elementi del tentativo sono corretti sia per valore che per posizione.
-     - Punteggi bianchi: indicano quanti elementi del tentativo sono corretti per valore ma non per posizione.
-
-  4. Il gioco continua finché il decodificatore non indovina esattamente il codice segreto o finché non esaurisce
-     i tentativi a disposizione.
-
   Implementazione del gioco:
 
   - La lunghezza del codice segreto verrà scelta dall'utente prima di iniziare una partita (4 - 6 - 8).
@@ -48,7 +30,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
+#include <ctype.h>  //per funzione tolower
 #include <time.h>
 
 #define FACILE 10
@@ -79,37 +61,45 @@ typedef struct struttura_giocatore
   int tutorial;
 } giocatore;
 
-void clear_input_buffer()
-{ // semplice funzione che serve per ipulire il buffer
-  int ch;
-  while ((ch = getchar()) != '\n' && ch != EOF){
-    getchar();
-  }
+void clear_input_buffer() { // semplice funzione che serve per ipulire il buffer
+    int ch;
+    do {
+        ch = getchar();
+    } while (ch != '\n' && ch != EOF);
 }
 
-int strcasecmp(const char *a, const char *b) 
-{
-  while (*a && *b)
-  {
-    if (tolower((unsigned char)*a) != tolower((unsigned char)*b))
-    {
-      return tolower((unsigned char)*a) - tolower((unsigned char)*b);
+int strcasecmp(char stringa1[], char stringa2[]) {
+    // Converti stringa1 in minuscolo
+    for (int i = 0; stringa1[i] != '\0'; i++) {
+        stringa1[i] = tolower(stringa1[i]);
     }
-    a++;
-    b++;
-  }
-  return tolower((unsigned char)*a) - tolower((unsigned char)*b);
+  
+    // Converti stringa2 in minuscolo
+    for (int i = 0; stringa2[i] != '\0'; i++) {
+        stringa2[i] = tolower(stringa2[i]);
+    }
+
+    // Confronta le stringhe e restituisci il risultato
+    int x = 0;
+    while (stringa1[x] != '\0' && stringa2[x] != '\0') {
+        if (stringa1[x] != stringa2[x]) {
+            return 1;
+        }
+        x++;
+    }
+    return 0; // Le stringhe sono uguali
 }
 
-void verifica_id( giocatore *player, char id[], char nomefile[]) // se ritorna 1 l'utente esiste già invece se ritorna 2 è stato creato un nuovo account
+void verifica_id(giocatore *player, char id[], char nomefile[]) // se ritorna 1 l'utente esiste già invece se ritorna 2 è stato creato un nuovo account
 {
 
   FILE *fp;
   char id_tmp[LUNGHEZZA_ID];
+  int trovato = 0;
 
   fp = fopen(nomefile, "r"); // apertura file in modalità lettura
 
-  printf("ho aperto il file");
+  printf("ho aperto il file\n");
 
   if (fp == NULL)
   {
@@ -117,14 +107,14 @@ void verifica_id( giocatore *player, char id[], char nomefile[]) // se ritorna 1
     exit(1);
   }
   // Leggi il file blocco per blocco
-  int trovato = 0;
-  while (fscanf(fp, "id= %10s\n", id_tmp) == 1 && !feof(fp))
+
+  while (fscanf(fp, "id= %s\n", id_tmp) == 1 && !feof(fp))
   { // legge finchè non trova un campo id e finchè non trova il campo eof (end of file)
     if (strcasecmp(id, id_tmp) == 0)
     { // chiamo la funzione di confronto non case sensitive, se il valore di ritorno è 0 allora leggo e salvo dati utente
       // Trovato l'utente, leggi le informazioni
 
-      printf("ho trovato l'id");
+      printf("ho trovato l'id\n");
 
       strcpy(player->id, id_tmp); // siccome non è possibile passare direttamente l'array id_tmp all'id del player lo copiamo con strcpy
       fscanf(fp, "games-played= %d\n", &player->partite_giocate);
@@ -132,11 +122,13 @@ void verifica_id( giocatore *player, char id[], char nomefile[]) // se ritorna 1
       fscanf(fp, "points= %d\n", &player->punti);
       fscanf(fp, "tutorial= %d\n", &player->tutorial);
 
-      printf("ho salvato i dati");
+      printf("ho salvato i dati\n");
 
       fclose(fp);
-      
+
       printf("Utente trovato, sono stati recuperati i dati delle sessioni precedenti\n");
+      getchar();
+      system("clear");
       trovato = 1;
       break;
     }
@@ -154,58 +146,60 @@ void verifica_id( giocatore *player, char id[], char nomefile[]) // se ritorna 1
     }
   }
   fclose(fp);
+
   if (trovato == 0)
   {
     // arrivati a questo punto vuol dire che l'utente non esiste e bisogna crearlo
     char consenso;
-    printf("L'id utente da te inserito non è presente nella memoria.\n Vuoi creare un nuovo profilo?\n y/n");
+    printf("L'id utente da te inserito non è presente nella memoria.\n Vuoi creare un nuovo profilo?\n y/n\n\n");
     consenso = getchar();
     if (consenso == 'y')
     {
-      printf("ho aperto il file");
+      printf("ho aperto il file\n");
 
       fp = fopen(nomefile, "a"); // apertura file in modalità append
       strcpy(player->id, id);
-      fprintf(fp,"id= %10s\n", id);
+      fprintf(fp, "id= %10s\n", id);
       player->partite_giocate = 0;
       player->partite_vinte = 0;
       player->punti = 0;
-      fprintf(fp,"games-played= %d\n", 0);
-      fprintf(fp,"games-won= %d\n", 0);
-      fprintf(fp,"points= %d\n", 0);
-      fprintf(fp,"tutorial= %d\n", 1);
-      fprintf(fp,".\n");
+      fprintf(fp, "games-played= %d\n", 0);
+      fprintf(fp, "games-won= %d\n", 0);
+      fprintf(fp, "points= %d\n", 0);
+      fprintf(fp, "tutorial= %d\n", 1);
+      fprintf(fp, ".\n");
       fclose(fp);
       system("clear");
-      printf("Il tuo nuovo profilo utente è stato creato correttamente!!");
+      printf("Il tuo nuovo profilo utente è stato creato correttamente!!\n");
       clear_input_buffer();
     }
     else
     {
       system("clear");
-      printf("Alla prossima!!");
+      printf("Alla prossima!!\n");
       exit(1);
     }
   }
 }
 
-int settings_partita(int *lunghezza_codice, int *difficolta,  giocatore *player) // se ritorna 0 c'è un problema, se ritorna 1 tutto ok e difficoltà settata, se ritorna 2 vuole vedere le regole
+int settings_partita(int *lunghezza_codice, int *difficolta, giocatore *player) // se ritorna 0 c'è un problema, se ritorna 1 tutto ok e difficoltà settata, se ritorna 2 vuole vedere le regole
 {
   char consenso;
-  char tmp [12];
+  char tmp[20];
   system("clear");
 
   if (player->tutorial == 1)
   {
     printf("Non hai mai visto le regole, vuoi vederle?\n y/n\n");
-    consenso = getchar();
     clear_input_buffer();
+    consenso = getchar();
 
     while (consenso != 'y' && consenso != 'n')
     {
       printf("Scelta non valida! Riprovare\n");
-      consenso = getchar();
       clear_input_buffer();
+      consenso = getchar();
+      
     }
   }
 
@@ -224,9 +218,9 @@ int settings_partita(int *lunghezza_codice, int *difficolta,  giocatore *player)
     clear_input_buffer();
     printf("\n\n Ora devi scegliere la difficoltà di gioco.\n FACILE = 10 TENTATIVI\n INTERMEDIA = 8 TENTATIVI\n DIFFICILE = 6 TENTATIVI\n");
     scanf(" %11s", tmp);
-    clear_input_buffer();
+  
 
-    while (strcasecmp(tmp, "FACILE") != 1 || strcasecmp(tmp, "INTERMEDIA") != 1 || strcasecmp(tmp, "DIFFICILE") != 1)
+   while (strcasecmp(tmp, "FACILE") != 0 || strcasecmp(tmp, "INTERMEDIA") != 0 || strcasecmp(tmp, "DIFFICILE") != 0)
     {
       printf("Input non valido! Riprovare:\n");
       scanf(" %11s", tmp);
@@ -277,10 +271,12 @@ void genera_codice(int *codice, int *lunghezza)
   }
 }
 
-void visualizza_regole(char nomefile[],  giocatore *player)
+void visualizza_regole(char nomefile[], giocatore *player)
 {
   int carattere;
   system("clear");
+  player->tutorial = 0;
+
   FILE *fp;
   fp = fopen(nomefile, "r");
   if (fp == NULL)
@@ -297,14 +293,11 @@ void visualizza_regole(char nomefile[],  giocatore *player)
 
   fclose(fp);
 
-  player->tutorial = 0;
-
+  
   printf("\n\nPremi 'esc' per uscire:\n");
-  while ((carattere = getchar()) != ESC_CHAR)
-  {
-    carattere = getchar(); // Continui a leggere caratteri fino a che non viene premuto ESC
+  while ((carattere = getchar()) != ESC_CHAR) {
+        printf("volevi dire 'esc' ?\n");
   }
-
 }
 
 int menu()
@@ -315,7 +308,7 @@ int menu()
   printf("[2] REGOLE DEL GIOCO\n");
   printf("[3] STORICO DEL GIOCATORE\n");
   printf("[0] ESCI DAL GIOCO\n");
-  printf("\n Scelta: ");
+  printf("\nScelta: ");
 
   scanf("%d", &scelta);
 
@@ -334,10 +327,10 @@ int main()
 {
   system("clear");
 
-  struct giocatore *player;                                      // Dichiarazione del puntatore alla struttura
+  struct giocatore *player;           // Dichiarazione del puntatore alla struttura
   player = malloc(sizeof(giocatore)); // Allocazione di memoria per la struttura
 
-  int difficolta, lunghezza_codice, input_utente;
+  int difficolta, lunghezza_codice, input_utente, settings;
   char id_utente[LUNGHEZZA_ID]; // Buffer per 10 caratteri + terminatore nullo (\0) che viene aggiunto automaticamente alla fine dello scanf
 
   int *codice;
@@ -356,7 +349,6 @@ int main()
 
   /*printf("Stringa letta: '%s'\n", id_utente);*/
 
-  clear_input_buffer();
   verifica_id(player, id_utente, "data.txt");
 
   while (1)
@@ -371,8 +363,8 @@ int main()
       break;
 
     case 1:
-      clear_input_buffer();
-      int settings = settings_partita(&lunghezza_codice, &difficolta, player);
+      settings = settings_partita(&lunghezza_codice, &difficolta, player);
+
       if (settings == 2)
       {
         visualizza_regole("regole.txt", player);
@@ -383,7 +375,7 @@ int main()
         // funzione di gioco
       }
 
-      printf("lungheza del codice= %d\n difficoltà= %d\n",lunghezza_codice,difficolta);
+      printf("lungheza del codice= %d\n difficoltà= %d\n", lunghezza_codice, difficolta);
       clear_input_buffer();
       getchar();
       clear_input_buffer();
